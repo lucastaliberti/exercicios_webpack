e0a7354638e94c16995f509776839e95cc3d7da9
Lucas Taliberti de Sousa <lucastaliberti@syllus.com.br>
2017-04-14 20:14:27 -0300

    Added css configuration on web pack

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,5 @@
 const webpack = require('webpack')
-
+const ExtractTextPlugin = require('extract-text-webpack-plugin')
 module.exports = {
     entry: './ex/index.js'
     , output: {
@@ -10,6 +10,9 @@ module.exports = {
         port: 8080
         , contentBase: './public'
     }
+    , plugins: [
+        new ExtractTextPlugin('app.css')
+    ]
     , module: {
         loaders: [{
             test: /.js$/
@@ -19,6 +22,10 @@ module.exports = {
                 presets: ['es2015', 'react']
                 , plugins: ['transform-object-rest-spread']
             }
+        }
+        , {
+            test: /\.css$/,
+            loader: ExtractTextPlugin.extract('style-loader', 'css-loader')
         }]
     }
 }
